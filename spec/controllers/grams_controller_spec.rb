b9d29542aaeb3467066364f92a6f6c5f392d6d9e
Tyna Huynh <tynahuynh@gmail.com>
2017-04-11 06:12:12 +0000

    added test for detail page

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe "grams#show action" do
+    it "should successfully show the page if the gram is found" do
+      gram = FactoryGirl.create(:gram)
+      get :show, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
+    end
+    it "should return a 404 error if the gram is not found" do
+      get :show, params: { id: 'TACOCAT'}
+      expect(response).to have_http_status(:not_found)
+    end
+
+
+
   describe "grams#index action" do
     it "should successfully show the page" do
       get :index
@@ -45,9 +58,10 @@ RSpec.describe GramsController, type: :controller do
       user = FactoryGirl.create(:user)
       sign_in user
 
+      gram_count = Gram.count
       post :create, params: {gram: {message: ''}}
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(Gram.count).to eq 0
+      expect(gram_count).to eq Gram.count
     end
 
   end
