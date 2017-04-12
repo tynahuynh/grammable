79c289ef1f00f6b02dfbf8386a1ac1e8121fdb85
Tyna Huynh <tynahuynh@gmail.com>
2017-04-12 06:07:46 +0000

    added test for destroy

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe "grams#destroy action" do
+    it "should allow users to destroy grams"do
+      gram = FactoryGirl.create(:gram)
+      delete :destroy, params: { id: gram.id }
+      expect(response).to redirect_to root_path
+      gram = Gram.find_by_id(gram.id)
+      expect(gram).to eq nil
+    end
+
+    it "should have http 404 error if we cannot find a gram  with the id that is specified" do
+      delete :destroy, params: { id: "SPACEDUCK"}
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
   describe "grams#update action" do
     it "should allow users to successfully update grams" do
       gram = FactoryGirl.create(:gram, message: "Initial Value")
