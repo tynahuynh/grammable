98c4265969334f0a5d251d1d2df2e005380ec64e
Tyna Huynh <tynahuynh@gmail.com>
2017-04-16 10:33:02 +0000

    styling

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -60,7 +60,5 @@ private
     params.require(:gram).permit(:message, :picture)
   end
 
-  def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
-  end
+
 end
