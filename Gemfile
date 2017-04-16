eb9cbb99eae4adfddd47aee6f90af9d699c7fda0
Tyna Huynh <tynahuynh@gmail.com>
2017-04-16 05:23:48 +0000

    added carrierwave

--- Gemfile
+++ Gemfile
@@ -60,3 +60,6 @@ end
 gem 'bootstrap', '~> 4.0.0.alpha6'
 gem 'simple_form'
 gem 'devise'
+gem 'carrierwave', '~> 1.0'
+gem "figaro"
+gem "fog"
--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -37,7 +37,7 @@ class GramsController < ApplicationController
   end
 
   def index
-
+    @grams = Gram.all
   end
 
   def new
@@ -57,7 +57,7 @@ class GramsController < ApplicationController
 private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
   def render_not_found(status=:not_found)
